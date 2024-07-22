8887b9ad36937784ebac9556ca94053d172393b1
ElifNidaKarakas <elifnidakarakas@gmail.com>
2024-07-23 00:40:35 +0300

    project case methods were created

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -2,7 +2,9 @@ package Pages;
 
 import Base.BaseLibrary;
 import org.openqa.selenium.By;
-import org.openqa.selenium.devtools.v85.browser.model.Bucket;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class LoginPage extends BaseLibrary {
 
@@ -22,23 +24,58 @@ public class LoginPage extends BaseLibrary {
         return this;
     }
 
-    public LoginPage locationFilter() {
-        webDriver.findElement(By.xpath("//*[@id=\"top-filter-form\"]/div[1]/span/span[1]/span/span[2]")).click();
+    public LoginPage locationDropdown() {
+        webDriver.findElement(By.xpath("//span[@id='select2-filter-by-location-container']")).click();
         return this;
     }
 
     public LoginPage locationClick() {
-        webDriver.findElement(By.cssSelector("#select2-filter-by-location-result-d323-Istanbul\\,\\ Turkey")).click();
+        webDriver.findElement((By.id("select2-filter-by-location-container"))).click();
+        //webDriver.findElement(By.cssSelector("#select2-filter-by-location-result-7c4y-Istanbul\\,\\ Turkey")).click();
         return this;
     }
 
-    public LoginPage departmentFilter(){
+    public LoginPage departmentDropdown() {
         webDriver.findElement(By.xpath("//*[@id=\"select2-filter-by-department-container\"]")).click();
         return this;
     }
 
-    public LoginPage departmentClick(){
-        webDriver.findElement(By.id("id=select2-filter-by-department-container")).click();
+    public LoginPage departmentClick() {
+        webDriver.findElement(By.id("select2-filter-by-department-container")).click();
+        return this;
+    }
+
+    public LoginPage viewRoleButton() {
+        webDriver.findElement(By.xpath("//*[@id=\"jobs-list\"]/div[1]/div/a")).click();
+        return this;
+    }
+
+    public LoginPage position() {
+        webDriver.findElement(By.xpath("//*[@id=\"jobs-list\"]/div[1]/div")).click();
+        return this;
+    }
+
+
+    public LoginPage jobList(){
+        // Filtreleme sonrası gelen sonuçları bul
+        List<WebElement> jobListings = webDriver.findElements(By.xpath("//*[@id=\"jobs-list\"]"));
+        // Kontrol edilmesi gereken kriterler
+        String expectedCity = "Istanbul, Turkey";
+        String expectedJobTitle = "Quality Assurance";
+        boolean allResultsMatchCriteria = true;
+        for (WebElement job : jobListings) {
+            String city = job.findElement(By.className("[position-department text-large font-weight-600 text-primary]")).getText();
+            String jobTitle = job.findElement(By.className("[position-location text-large]")).getText();
+            if (!city.equals(expectedCity) || !jobTitle.equals(expectedJobTitle)) {
+                allResultsMatchCriteria = false;
+                break;
+            }
+        }
+        if (allResultsMatchCriteria) {
+            System.out.println("Tüm sonuçlar belirlenen kriterlere uygun.");
+        } else {
+            System.out.println("Bazı sonuçlar belirlenen kriterlere uygun değil.");
+        }
         return this;
     }
 }
--- src/test/java/InsiderTest.java
+++ src/test/java/InsiderTest.java
@@ -7,8 +7,16 @@ public class InsiderTest extends BaseTest {
     MainPage mainPage = new MainPage();
     LoginPage loginPage = new LoginPage();
 
+    //Visit https://useinsider.com/ and check Insider home page is opened or not
+    @Test(description = "Insider Buton Kontrolü")
+    public void insiderLinkControl() throws InterruptedException {
+        webDriver.get("https://useinsider.com/");
+        sleep(2000);
+    }
+
+    //Select the “Company” menu in the navigation bar, select “Careers” and check Career page, its Locations, Teams, and Life at Insider blocks are open or not
     @Test(description = "Company Buton Kontrolü")
-    public void companyButonKontrol() throws InterruptedException {
+    public void companyButonControl() throws InterruptedException {
         webDriver.get("https://useinsider.com/");
         sleep(2000);
         mainPage.acceptAllButonClick();
@@ -19,6 +27,8 @@ public class InsiderTest extends BaseTest {
 
     }
 
+    //Go to https://useinsider.com/careers/quality-assurance/, click “See all QA jobs”, filter jobs by Location:
+    // “Istanbul, Turkey”, and Department: “Quality Assurance”, check the presence of the job list
     @Test(description = "Şehir ve meslek filtreleme")
     public void filterJob() throws InterruptedException {
         webDriver.get("https://useinsider.com/careers/quality-assurance/");
@@ -26,14 +36,53 @@ public class InsiderTest extends BaseTest {
         mainPage.acceptAllButonClick();
         sleep(1000);
         loginPage.qaAllJob();
+        sleep(20000);
+        loginPage.locationDropdown();
         sleep(2000);
-        loginPage.locationFilter();
+        loginPage.locationClick();   //turkiye seçmiyorlisteyi açıyor all olarak alıyor yine ----------------------
         sleep(1000);
-        loginPage.locationClick();
-        sleep(1000);
-        loginPage.departmentFilter();
+        loginPage.departmentDropdown();
         sleep(2000);
         loginPage.departmentClick();
+        sleep(10000);
+    }
+
+    //Check that all jobs’ Position contains “Quality Assurance”, Department contains “Quality Assurance”, and Location contains “Istanbul, Turkey”
+    @Test
+    public void jobListControl() throws InterruptedException {
+        webDriver.get("https://useinsider.com/careers/quality-assurance/");
+        sleep(2000);
+        mainPage.acceptAllButonClick();
+        sleep(1000);
+        loginPage.qaAllJob();
+        sleep(20000);
+        loginPage.locationDropdown();
+        sleep(2000);
+        loginPage.locationClick();   //turkiye seçmiyorlisteyi açıyor all olarak alıyor yine ----------------------
+        sleep(1000);
+        loginPage.departmentDropdown();
+        sleep(2000);
+        loginPage.departmentClick();
+        sleep(10000);
+        loginPage.jobList();
+        sleep(1000);
+
 
     }
+
+    //Click the “View Role” button and check that this action redirects us to the Lever Application form page
+    @Test(description = "View Role button link kontrolü")
+    public void voiwRollerButtonLinkControl() throws InterruptedException {
+        webDriver.get("https://useinsider.com/careers/quality-assurance/");
+        sleep(2000);
+        mainPage.acceptAllButonClick();
+        sleep(1000);
+        loginPage.qaAllJob();
+        sleep(2000);
+        loginPage.position();
+        sleep(1000);
+        loginPage.viewRoleButton();
+        sleep(5000);
+    }
+
 }
